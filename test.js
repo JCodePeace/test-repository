abdd9c72849123d6363291d37159de727b19b94e
JCodePeace <mongolzzz21@gmail.com>
2025-05-02 12:38:36 +0300

    Added new url

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function send() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  fetch('http://localhost:3000/register', {
+  fetch('https://test-repository-s8b9.onrender.com/register', {
     method: 'POST',
     headers: { 'Content-type': 'application/json' },
     body: JSON.stringify({ username, password })
@@ -10,7 +10,7 @@ function send() {
 }
 
 function getUsers() {
-  fetch('http://localhost:3000/users')
+  fetch('https://test-repository-s8b9.onrender.com/users')
     .then(req => req.json())
     .then(response => {
       document.getElementById('users').innerHTML = '';
@@ -24,7 +24,7 @@ function login() {
   const username = document.getElementById("login_username").value;
   const password = document.getElementById("login_password").value;
   
-  fetch('http://localhost:3000/login', {
+  fetch('https://test-repository-s8b9.onrender.com/login', {
     method: 'POST',
     headers: { 'Content-type': 'application/json' },
     body: JSON.stringify({ username, password })
